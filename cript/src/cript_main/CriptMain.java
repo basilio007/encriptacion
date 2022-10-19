418723bd1126b5b91ee1da858ce23c3a7bb55209
basilio007 <113118989+basilio007@users.noreply.github.com>
2022-10-19 20:12:30 +0200

    encriptacionMensajes

---
+++ cript/src/cript_main/CriptMain.java
@@ -0,0 +1,10 @@
+package cript_main;
+
+public class CriptMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println();
+	}
+
+}
---
+++ cript/src/cript_object/CriptObject.java
@@ -0,0 +1,11 @@
+package cript_object;
+
+import java.util.Scanner;
+
+public class CriptObject {
+String letras = "ABCDEFGHIJKLMNSTUVWXYZ";
+String mensajeOriginal, mensageEncriptado = "";
+char letramensage ;
+
+Scanner lector = new Scanner(System.in);
+}
