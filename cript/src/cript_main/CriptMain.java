0e78169444e5462a3146173bbfa674eecb15f2f3
basilio007 <113118989+basilio007@users.noreply.github.com>
2022-10-21 19:33:59 +0200

    go

--- cript/src/cript_main/CriptMain.java
+++ cript/src/cript_main/CriptMain.java
@@ -1,10 +1,43 @@
 package cript_main;
 
+import java.util.Scanner;
+
 public class CriptMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println();
+		Scanner lector = new Scanner(System.in);
+		
+		String letras = "ABCDEFGHIJKLMNSTUVWXYZ";
+		String frase ;
+System.out.println("introduce frase");
+frase=lector.nextLine();
+
+
+
 	}
+	public static String codificar(String letras,String frase ){
+		String textodescodificado ="";
+		frase = frase.toUpperCase();
+		char caracter;
+		for(int i=0; i<letras.length(); i++) {
+			caracter= frase.charAt(i);
+			int posicion = letras.indexOf(caracter);
+			if (posicion == -1) {
+				
+			
+				textodescodificado+=caracter;
+				
+				
+			}else {
+				textodescodificado+=letras.charAt((posicion+3) % letras.length());
+			}
+			
+			
+		}
+		return textodescodificado;
+		
+	}
+	
 
 }
--- cript/src/cript_object/CriptObject.java
+++ cript/src/cript_object/CriptObject.java
@@ -3,9 +3,8 @@ package cript_object;
 import java.util.Scanner;
 
 public class CriptObject {
-String letras = "ABCDEFGHIJKLMNSTUVWXYZ";
-String mensajeOriginal, mensageEncriptado = "";
-char letramensage ;
 
-Scanner lector = new Scanner(System.in);
+
+
+
 }
