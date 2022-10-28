bbf8bde8e8db6492bb5d755ac9d02b08a144457e
basilio007 <113118989+basilio007@users.noreply.github.com>
2022-10-28 18:07:27 +0200

    GOIN

--- cript/src/cript_main/CriptMain.java
+++ cript/src/cript_main/CriptMain.java
@@ -8,72 +8,14 @@ public class CriptMain {
 
 	public static void main(String[] args) {
 		
-		CriptObject prueba = new CriptObject();
 		
-		prueba.lectordatos();
-		System.out.println(prueba.encriptar());
 		
-		/*Scanner lector = new Scanner(System.in);
-		lector.useDelimiter("/n");
-		String letras = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
-		String frase;
-		System.out.println("introduce frase");
-		frase = lector.nextLine();
-
-		String texto = codificar(letras, frase);
-		System.out.println("El texto " + frase + " encriptado en metodo Cesar es: " + texto);
-		System.out.println("--------------------------");
-		texto = descodificar(letras, texto);
-		System.out.println("descodificado: " + texto);
-
+		CriptObject text = new CriptObject();	
+		
+		text.lectordatos();
+		
+		System.out.println(text.encriptar());
+		
 	}
-
-	public static String codificar(String letras, String frase) {
-		String textcodificado = "";
-		frase = frase.toUpperCase();
-
-		char caracter;
-		for (int i = 0; i < frase.length(); i++) {
-			caracter = frase.charAt(i);
-			int posicion = letras.indexOf(caracter);
-			if (posicion == -1) {
-
-				textcodificado += caracter;
-
-			} else {
-				textcodificado += letras.charAt((posicion + 3) % letras.length());
-			}
-
-		}
-		return textcodificado;
-
-	}
-
-	public static String descodificar(String letras, String frase) {
-		String textdescodificado = "";
-		frase = frase.toUpperCase();
-		char caracter;
-		for (int i = 0; i < frase.length(); i++) {
-			caracter = frase.charAt(i);
-			int posicion = letras.indexOf(caracter);
-			if (posicion == -1) {
-
-				textdescodificado += caracter;
-
-			} else {
-				if (posicion - 3 < 0) {
-
-					textdescodificado += letras.charAt(letras.length() + (posicion - 3));
-				} else {
-					textdescodificado += letras.charAt(posicion - 3) % (letras.length());
-				}
-			}
-
-			
-
-		}*/
-
-//		return textdescodificado;
-	}
-
 }
+
--- cript/src/cript_object/CriptObject.java
+++ cript/src/cript_object/CriptObject.java
@@ -8,72 +8,43 @@ public class CriptObject {
 	private int clave;
 	private static char alfabeto[] ={'A','B','C','D','E','F','G','H','I','J','K','L','M',
 									 'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-
 	public void cript_object() {
 		
 	}
-	public CriptObject() {
-		
-		this.frase = frase;
-		this.clave = clave;
-	}
-	public void CriptObject(String frase, int clave) {
-		
-		this.frase = frase;
-		this.clave = clave;
-	}
-	public String getFrase() {
-		return frase;
-	}
-	public void setFrase(String frase) {
-		this.frase = frase;
-	}
-	public int getClave() {
-		return clave;
-	}
-	public void setClave(int clave) {
-		this.clave = clave;
-	}
-	public static char[] getAlfabeto() {
-		return alfabeto;
-	}
-	public static void setAlfabeto(char[] alfabeto) {
-		CriptObject.alfabeto = alfabeto;
-	}
 	public void lectordatos() {
 		Scanner lector2 = new Scanner(System.in);
-		System.err.println("introduce una frase");
-		this.frase=lector2.nextLine().toUpperCase(null);
+		System.out.println("introduce una frase");
+		frase=lector2.nextLine().toUpperCase();
 		
 		System.out.println("introduce la clave");
-		this.clave=lector2.nextInt();
+		clave=lector2.nextInt();
 				
 		lector2.close();
 				}
 public char[] encriptar() {
 	
-	char[] mensajeEncriptado = new char [getFrase().length()];
+	char[] mensajeEncriptado = new char [frase.length()];
 	
-	for(int i=0; i<this.getFrase().length(); i++) {
-		if(getFrase().charAt(i)==' ') {
-			mensajeEncriptado[i]=getFrase().charAt(i);
+	for(int i=0; i<frase.length(); i++) {
+		if(frase.charAt(i)==' ') {
+			mensajeEncriptado[i]=frase.charAt(i);
 		}else {
-			for(int j=0; j< getAlfabeto().length; j++ ) {
-				if(getFrase().charAt(i)==getAlfabeto()[j]) {
+			for(int j=0; j< alfabeto.length; j++ ) {
+				if(frase.charAt(i)== alfabeto[j]) {
 					
-					if(getClave()<0) {
-						mensajeEncriptado[i]=getAlfabeto()[(j+getClave()+getAlfabeto().length)%getAlfabeto().length];	
-						j=getAlfabeto().length;
+					if(clave<0) {
+						mensajeEncriptado[i]=alfabeto[(j+clave+alfabeto.length)%alfabeto.length];	
+						j=alfabeto.length;
 						
 					}else {
-						mensajeEncriptado[i]=getAlfabeto()[(j+getClave())%getAlfabeto().length];	
-						j=getAlfabeto().length;
+						mensajeEncriptado[i]=alfabeto[(j+clave)%alfabeto.length];	
+						j=alfabeto.length;
 						
 					}
 					
 					
 				}else {
-					mensajeEncriptado[i]=getFrase().charAt(i);
+					mensajeEncriptado[i]=frase.charAt(i);
 				}
 				
 			}
